f4f7d39f43e46f5164f9e11a27b900b90d8ff607
Taufiq Muhammadi <waruboy@yahoo.com>
2013-03-19 16:24:01 +0700

    terima email pakai mailgun

--- surel/views.py
+++ surel/views.py
@@ -1,19 +1,24 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from .libs import parse_emailyak
 from .models import Surel
 
 
 @csrf_exempt
 def suara_masuk(request):
 	if request.method == 'POST':
-		email = parse_emailyak(request)
+		sender    = request.POST.get('sender')
+		recipient = request.POST.get('recipient')
+		subject   = request.POST.get('subject', '')
+		body_plain = request.POST.get('body-plain', '')
+		body_without_quotes = request.POST.get('stripped-text', '')
+		# attachments:
+		for key in request.FILES:
+			file = request.FILES[key]
 		surel_masuk = Surel.objects.create(
-			sender = email['pengirim'],
-			recipient = email['penerima'],
+			sender = sender,
+			recipient = recipient,
 			body_plain = email['body_plain'],
 			)
-		respon = proses_surel(surel_masuk)
-		return respon
+		return HttpResponse('OK')
 	return HttpResponse('Tempat nerima email')
